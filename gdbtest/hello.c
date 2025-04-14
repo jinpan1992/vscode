a13380e9c6025939b5240fa1bb9f22fc90b5d536
jinpan <1574985762@qq.com>
2025-04-14 10:36:12 +0800

    gcc编译 gdb调试单个文件

---
+++ gdbtest/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    printf("hello jinpan!\n");
+    return 0;
+}
---
+++ gdbtest/testPRJ/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World111!\n");
+    printf("hello jinpan111!\n");
+    return 0;
+}
